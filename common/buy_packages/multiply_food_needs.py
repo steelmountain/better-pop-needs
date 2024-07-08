4b4bc0edd747052a1f169e5184b9f71f5d31fef4
Steelmountain <steelmountaina@gmail.com>
2024-07-08 17:49:35 +0200

    feat: updated for 1.7, multiplied pop_need_service by x1.5

--- common/buy_packages/multiply_food_needs.py
+++ common/buy_packages/multiply_food_needs.py
@@ -1,25 +1,20 @@
 import re
 
 print("A POP DEMAND TWEAK")
-pop_need_multiplier = 2
+pop_need_multiplier = 1.5
 # pop_need_multiplier = input("hello, please add multiplier: ")
 
 new_lines = []
-with open("A_00_buy_packages.backup_txt", 'r') as file_pop_needs:
+with open("TT_price_employ_buy_packages.txt", 'r') as file_pop_needs:
      Lines = file_pop_needs.readlines()
      
      for line in Lines:
-        if  "popneed_basic_food" in line and line != "":
-            test = re.findall("\\d+",line)
-            number = test.pop()
-            number = int(number) * pop_need_multiplier
-            line = re.sub("\\d+", str(number), line)
-        if  "popneed_luxury_food" in line and line != "":
+        if  "popneed_services" in line and line != "":
             test = re.findall("\\d+",line)
             number = test.pop()
             number = int(number) * pop_need_multiplier
             line = re.sub("\\d+", str(number), line)
         new_lines.append(line)
-f= open("00_price_employ_buy_packages.txt","w+")
+f= open("TT_1_price_employ_buy_packages.txt","w+")
 f.writelines(new_lines)
 
